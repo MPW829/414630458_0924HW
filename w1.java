028e2e09aa71f58557b2cac55db3d9d057bfc638
MPW829 <tiffanyma007@gmail.com>
2025-10-01 08:47:52 +0800

    Add files via upload

---
+++ w1.java
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>自我介紹</title>
+</head>
+<body>
+
+<div style="background-color: pink; padding: 10px;">
+  <h2>自我介紹</h2>
+  <p>我叫馬沛彣，來自中正高中，但住在淡水，所以學校離我家算近。</p>
+</div>
+
+<div style="background-color: lightgreen; padding: 10px;">
+  <h2>喜好項目</h2>
+  <p>我的興趣是看電影、打羽球與畫圖，其中 <span style="color: red;">畫圖是我一直有持續進行的興趣</span>。只要有空閒的時間都會拿出電繪板，期望自己能更加進步。</p>
+</div>
+
+
+
+</body>
+</html>
